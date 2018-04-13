d9b89de8211d6bcff3d946e1a06b07d9f8ad11fe
Tiilikainen Pekka <pekka.tiilikainen@digia.com>
2018-04-13 10:48:50 +0300

    Adds recognizing user inputs for certain letters

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const readline = require('readline')
 readline.emitKeypressEvents(process.stdin)
 process.stdin.setRawMode(true)
 
+const userInputs = new Map()
+userInputs.set('a', 'ADD')
+userInputs.set('r', 'RESUME')
+userInputs.set('s', 'STOP')
+
 process.stdin.on('keypress', (str, key) => {
     if (key.ctrl && key.name === 'c') {
         process.exit()
+    } else if (userInputs.has(str)) {
+        const command = userInputs.get(str)
+        console.log(`Running command "${command}"`)
     } else {
-        console.log(`you pressed the "${str}" key`)
+        console.log(`No command found for "${str}" key`)
     }
 })
